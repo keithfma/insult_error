35e1daba76157eb26a71b0645bd386fb6bdef269
Keith Ma <keithfma@gmail.com>
2018-10-26 00:59:34 -0400

    Added fields to setup, added MIT license

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup
 setup(
     name="insult_error",
     version="0.1.0",
+    description='Intentionally insulting exceptions',
     author="Keith Ma",
     author_email="itsallnans@gmail.com",
+    url='https://github.com/keithfma/insult_error',
     py_modules=['insult_error']
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        ]
     )
