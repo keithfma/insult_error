fb7de9b90647c738944adb963c55acba001eb965
Keith Ma <keithfma@gmail.com>
2018-10-26 00:52:07 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="insult_error",
-    version="0.0.0",
+    version="0.1.0",
     author="Keith Ma",
     author_email="itsallnans@gmail.com",
     py_modules=['insult_error']
